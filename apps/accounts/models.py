27120b9941707f786f9f38867e00ed0e0df7f2aa
sptcnl <sptcnl@gmail.com>
2025-01-30 00:28:01 +0900

    update: username 보이도록 함

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     storage_limits = models.IntegerField(default=10)
     money = models.IntegerField(default=20000)
+
+    def __str__(self):
+        return self.username
