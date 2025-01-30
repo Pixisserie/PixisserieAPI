040a69e2043b2b8e2979df1c90d4daeb4fb23b19
sptcnl <sptcnl@gmail.com>
2025-01-31 02:14:15 +0900

    fix: settings 통일

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,22 @@ environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
 )
 
+SECRET_KEY = env('PROD_SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '(pythonanywhere ID).pythonanywhere.com', 
+    '127.0.0.1',
+    'localhost',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Application definition
 
