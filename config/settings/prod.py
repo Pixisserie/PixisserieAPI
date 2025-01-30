64504346474e6beaa4665dc5d7601c02c6ed5b73
sptcnl <sptcnl@gmail.com>
2025-01-31 01:09:56 +0900

    update: 배포 준비

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,4 +4,11 @@ SECRET_KEY = env('PROD_SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['(pythonanywhere ID).pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
