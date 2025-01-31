b3457dd20c81c01f8a4792cf4e51e63780d5341c
sptcnl <sptcnl@gmail.com>
2025-02-01 00:59:36 +0900

    fix: 로그인, 회원가입 response에 필드 추가

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('check/username/', views.CheckUsernameView.as_view()),
     path('signup/', views.SignupView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),
+    path('login/', views.LoginView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework.exceptions import ValidationError
 import logging
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 
 logger = logging.getLogger("accounts.views")
@@ -21,35 +22,53 @@ class CheckUsernameView(APIView):
         username = request.query_params.get('username')
         
         if not username:
-            return Response({"error": "username을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Please enter a username."}, status=status.HTTP_400_BAD_REQUEST)
         
         is_exist = User.objects.filter(username=username).exists()
 
         if is_exist:
-            return Response({"available": False, "message": "해당 유저네임이 이미 존재합니다."}, status=status.HTTP_200_OK)
+            return Response({"available": False, "message": "This username is already in use."}, status=status.HTTP_200_OK)
         else:
-            return Response({"available": True, "message": "유저네임 설정 가능"}, status=status.HTTP_200_OK)
+            return Response({"available": True, "message": "This username is available."}, status=status.HTTP_200_OK)
 
 @permission_classes([AllowAny])
 class SignupView(APIView):
     def post(self, request):
+        username = request.data['username']
+        is_username_exist = User.objects.filter(username=username).exists()
+        if is_username_exist:
+            raise ValidationError({'message': 'This username is already in use.'})
         serializer = SignupSerializer(data=request.data)
         try:
             # 객체 저장 시도
-            serializer.is_valid()
-            user = serializer.save()
-            logger.info("Successfully created an object.")
+            if serializer.is_valid(raise_exception=True):
+                user = serializer.save()
+                logger.info("Successfully created an object.")
 
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            refresh_token = str(refresh)
+                refresh = RefreshToken.for_user(user)
+                access_token = str(refresh.access_token)
+                refresh_token = str(refresh)
 
-            return Response({
-                "refresh": refresh_token,
-                "access": access_token,
-            }, status=status.HTTP_201_CREATED)
+                return Response({
+                    "username": str(user.username),
+                    "refresh": refresh_token,
+                    "access": access_token,
+                }, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             # 예외 발생 시 로그 기록
             logger.error(f"Error occurred while creating an object: {str(e)}")
-            raise ValidationError({"detail": f"An error occurred: {str(e)}"})
+            raise ValidationError({"error": f"An error occurred: {str(e)}"})
+
+@permission_classes([AllowAny])
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user:
+            return Response({'message': 'The username or password does not match'}, status=status.HTTP_400_BAD_REQUEST)
+        refresh = RefreshToken.for_user(user)
+        return Response({'username': str(user.username),
+                        'refresh_token': str(refresh),
+                        'access_token': str(refresh.access_token)}, status=status.HTTP_200_OK)
